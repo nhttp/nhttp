2706917250233d056b728e0905dba199cb669b2e
herudi <herudi7@gmail.com>
2021-05-31 10:41:15 +0700

    examples use deno fmt

--- examples/send-file.ts
+++ examples/send-file.ts
@@ -2,19 +2,25 @@ import { NHttp } from "../mod.ts";
 import { contentType } from "https://deno.land/x/media_types/mod.ts";
 
 class FileResponse extends Response {
-    constructor(pathFile: string, opts: ResponseInit & { baseDir?: string } = {}) {
-        opts.baseDir = (opts.baseDir || Deno.cwd()) as string;
-        opts.headers = (opts.headers || new Headers()) as Headers;
-        const extension = pathFile.substring(pathFile.lastIndexOf('.') + 1);
-        opts.headers.set('content-type', contentType(extension) || "application/octet-stream");
-        super(Deno.readFileSync(opts.baseDir + pathFile), opts);
-    }
+  constructor(
+    pathFile: string,
+    opts: ResponseInit & { baseDir?: string } = {},
+  ) {
+    opts.baseDir = (opts.baseDir || Deno.cwd()) as string;
+    opts.headers = (opts.headers || new Headers()) as Headers;
+    const extension = pathFile.substring(pathFile.lastIndexOf(".") + 1);
+    opts.headers.set(
+      "content-type",
+      contentType(extension) || "application/octet-stream",
+    );
+    super(Deno.readFileSync(opts.baseDir + pathFile), opts);
+  }
 }
 
 const app = new NHttp();
 
 app.get("/send-file", (request, respondWith) => {
-  respondWith(new FileResponse('/public/test.css'));
+  respondWith(new FileResponse("/public/test.css"));
 });
 
 app.listen(3000, () => {
