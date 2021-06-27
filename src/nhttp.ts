968a93d7eb5f51cd0fce7d9b191407a930c75702
herudi <herudi7@gmail.com>
2021-06-27 23:04:53 +0700

    add request event

--- src/nhttp.ts
+++ src/nhttp.ts
@@ -351,6 +351,20 @@ export class NHttp<
     url.search = search;
     rev._parsedUrl = url;
   };
+  fetchRequestEvent = () => {
+    return {
+      handleEvent: async (event: any) => {
+        let resp: (res: Response) => void;
+        const promise = new Promise<Response>((ok) => (resp = ok));
+        const rw = event.respondWith(promise);
+        this.handle({
+          request: event.request,
+          respondWith: resp!,
+        } as any);
+        await rw;
+      },
+    }
+  }
 }
 
 export const fetchRequestEvent = (app: NHttp) => {
