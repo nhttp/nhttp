a6600f73b66d7c550e56444965e70b1e2c51c41b
herudi <herudi7@gmail.com>
2021-06-28 07:03:35 +0700

    examples

--- examples/deno-deploy.ts
+++ examples/deno-deploy.ts
@@ -1,11 +1,9 @@
-import { NHttp, fetchEventHandler } from "https://deno.land/x/nhttp@0.6.0/mod.ts";
+import { NHttp } from "https://deno.land/x/nhttp@0.7.0/mod.ts";
 
 const app = new NHttp();
 
 app.get("/", ({ response }) => {
-    response.type("text/html").send(`
-        <h1>Hello from <a href="https://deno.land/x/nhttp">NHttp</a></h1>
-    `)
+    response.send("Hello deploy")
 });
 
-addEventListener("fetch", fetchEventHandler(app));
+addEventListener("fetch", app.fetchEventHandler());
