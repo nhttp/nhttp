978eb2886ad2ef02ea2f90433a3a596b9bb6087d
herudi <herudi7@gmail.com>
2022-01-21 15:19:13 +0700

    test

--- tests/api_test.ts
+++ tests/api_test.ts
@@ -1,13 +1,63 @@
 import { superdeno } from "https://deno.land/x/superdeno@4.7.2/mod.ts";
-import { NHttp } from "./../mod.ts";
+import { TObject } from "../src/types.ts";
+import {
+  expressMiddleware,
+  JsonResponse,
+  NextFunction,
+  NHttp,
+} from "./../mod.ts";
 
-const app = new NHttp();
+type EMidd = (
+  req: TObject,
+  _: TObject,
+  next: NextFunction,
+) => void | Promise<void>;
+
+const sleep = (ms: number) => new Promise((ok) => setTimeout(ok, ms));
+
+const app = new NHttp({
+  env: "production",
+  bodyLimit: {
+    json: "2mb",
+    urlencoded: "2mb",
+    raw: "2mb",
+    multipart: "100mb",
+  },
+});
+const handle = (request: Request) => app.handleEvent({ request });
+const handleApp = (myapp: NHttp) =>
+  (request: Request) => myapp.handleEvent({ request });
 
 // normal path
+app.use("/hello", () => "Hello");
 app.get("/", () => "Hello");
+
+// feature response
+app.get("/res", ({ response }) => {
+  response.type("text/html");
+  response.header("x-powered-by", "nhttp");
+  response.header({ "name": "john" });
+  response.header(new Headers({ "name1": "john" }));
+  response.cookie("key", "value");
+  response.cookie("key1", { "no": "1" });
+  response.clearCookie("key");
+  return "<h1>hello</h1>";
+});
+
+// async
+app.get("/async", async () => {
+  await sleep(1000);
+  return "Hello";
+});
 
 // send json
 app.get("/json", () => ({ name: "john" }));
+app.get("/json2", () =>
+  new JsonResponse({ name: "john" }, {
+    headers: new Headers({
+      "x-powered-by": "nhttp",
+    }),
+  }));
 
 // test query
 app.get("/users", ({ query }) => query);
@@ -18,6 +68,12 @@ app.get("/users/:usersId/books/:bookId", ({ params }) => params);
 // test params extension
 app.get("/image/:name.(jpg|png)", ({ params }) => params);
 
+// test error
+app.get("/noop500", ({ noop }) => {
+  noop();
+  return "noop";
+});
+
 // test regexp
 app.get(/\/hello/, () => "Hello Regexp");
 
@@ -27,18 +83,65 @@ app.post("/post", ({ response, body }) => {
   return body;
 });
 
-// add request from superdeno to requestEvent.
-const handle = (request: Request) => app.handleEvent({ request });
+app.on404(({ url }) => {
+  return {
+    status: 404,
+    message: `Route ${url} not found`,
+    name: "NotFoundError",
+  };
+});
 
+app.onError((err) => {
+  return {
+    status: err.status || 500,
+    message: err.message || "Something went wrong",
+    name: err.message || "InternalServerError",
+  };
+});
+
+Deno.test("it should USE hello response Hello", async () => {
+  await superdeno(handle)
+    .get("/hello")
+    .expect(200)
+    .expect("Hello");
+});
+Deno.test("it should GET/res feature response", async () => {
+  await superdeno(handle)
+    .get("/res")
+    .expect(200)
+    .expect("content-type", "text/html")
+    .expect("x-powered-by", "nhttp");
+});
 Deno.test("it should GET/ response Hello", async () => {
   await superdeno(handle)
     .get("/")
     .expect(200)
     .expect("Hello");
 });
+Deno.test("it should GET/async response Hello", async () => {
+  await superdeno(handle)
+    .get("/async")
+    .expect(200)
+    .expect("Hello");
+});
 Deno.test("it should GET/json response { name: 'john' }", async () => {
   await superdeno(handle)
     .get("/json")
+    .expect(200)
+    .expect("Content-Type", /^application/)
+    .expect({ name: "john" });
+});
+Deno.test("it should GET/json2 response { name: 'john' }", async () => {
+  await superdeno(handle)
+    .get("/json2")
+    .expect(200)
+    .expect("Content-Type", /^application/)
+    .expect("x-powered-by", "nhttp")
+    .expect({ name: "john" });
+});
+Deno.test("it should GET/json/ response { name: 'john' }", async () => {
+  await superdeno(handle)
+    .get("/json/")
     .expect(200)
     .expect("Content-Type", /^application/)
     .expect({ name: "john" });
@@ -78,6 +181,16 @@ Deno.test("it should route support RegExp", async () => {
   await superdeno(handle)
     .get("/hello-world")
     .expect(200);
+});
+Deno.test("it should route not found GET/noop404", async () => {
+  await superdeno(handle)
+    .get("/noop404")
+    .expect(404);
+});
+Deno.test("it should route GET/noop500 error", async () => {
+  await superdeno(handle)
+    .get("/noop500")
+    .expect(500);
 });
 Deno.test("it should route POST via content-type application/json", async () => {
   await superdeno(handle)
@@ -125,4 +238,80 @@ Deno.test("it should route POST via content-type multipart/form-data", async () 
         lastName: "Doe",
       },
     });
+});
+Deno.test("it should route POST via content-type text/plain", async () => {
+  await superdeno(handle)
+    .post("/post")
+    .set("Content-Type", "text/plain")
+    .set("Accept", "text/plain")
+    .send(JSON.stringify({
+      name: {
+        firstName: "John",
+        lastName: "Doe",
+      },
+    }))
+    .expect(201)
+    .expect({
+      name: {
+        firstName: "John",
+        lastName: "Doe",
+      },
+    });
+});
+Deno.test("it should middleware via expressMiddleware wrapper", async () => {
+  const sub = new NHttp()
+    .use(expressMiddleware([
+      (_, res, next) => {
+        res.set("name", "john");
+        if (res.hasHeader("name")) {
+          res.removeHeader("name");
+        }
+        res.setHeader("name", "john");
+        res.set("name", res.get("name"));
+        res.set("name", res.getHeader("name"));
+        res.writeHead(200, { name: "john" });
+        return next();
+      },
+      (req, res) => {
+        if (req.url === "/") {
+          return res.end("hello");
+        } else {
+          return req.respond({ body: "hello respond" });
+        }
+      },
+    ] as EMidd[]));
+  await superdeno(handleApp(sub))
+    .get("/")
+    .expect(200)
+    .expect("name", "john")
+    .expect("hello");
+  await superdeno(handleApp(sub))
+    .get("/hello")
+    .expect(200)
+    .expect("hello respond");
+});
+Deno.test("it should custom parseQuery", async () => {
+  const sub = new NHttp({
+    parseQuery: (str: string) =>
+      Object.fromEntries(new URLSearchParams(str).entries()),
+  })
+    .get("/hello", ({ query }) => query);
+  await superdeno(handleApp(sub))
+    .get("/hello?name=john")
+    .expect(200)
+    .expect({ name: "john" });
+});
+Deno.test("it should multiple error", async () => {
+  const sub = new NHttp()
+    .use((_, next) => {
+      return next(new Error("my error"));
+    })
+    .onError((err, { noop }) => {
+      noop();
+      return err.message || "hello";
+    });
+  await superdeno(handleApp(sub))
+    .get("/hello")
+    .expect(500)
+    .expect(/noop/);
 });
--- tests/internal_test.ts
+++ tests/internal_test.ts
@@ -2,7 +2,25 @@ import {
   assert,
   assertEquals as expect,
 } from "https://deno.land/std@0.105.0/testing/asserts.ts";
-import { Handler, NHttp, Router } from "./../mod.ts";
+import {
+  concatRegexp,
+  getReqCookies,
+  parseQuery,
+  serializeCookie,
+} from "../src/utils.ts";
+import {
+  getError,
+  Handler,
+  HttpError,
+  HttpResponse,
+  multipart,
+  NHttp,
+  RequestEvent,
+  Router,
+} from "./../mod.ts";
+
+const rev = new RequestEvent();
+const res = new HttpResponse();
 
 // deno-lint-ignore no-explicit-any
 type TObject = { [k: string]: any };
@@ -14,6 +32,29 @@ const fn: Handler = () => {};
 
 const router = new Router();
 const methods = ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"];
+test("concatRegexp instanceof RegExp", () => {
+  expect(concatRegexp("/hello", /\/hello/) instanceof RegExp, true);
+  expect(concatRegexp(/\/hello\//, /\/hello/) instanceof RegExp, true);
+});
+test("RequestEvent undefined first", () => {
+  const names = Object.getOwnPropertyNames(rev);
+  names.forEach((name) => {
+    expect(typeof rev[name], "undefined");
+  });
+});
+test("HttpResponse undefined first", () => {
+  const names = Object.getOwnPropertyNames(res);
+  names.forEach((name) => {
+    expect(typeof rev[name], "undefined");
+  });
+});
+test("getError object", () => {
+  const err = new HttpError(500, "my test error", "MyTestError") as TObject;
+  const obj = getError(err, true);
+  expect(typeof obj, "object");
+  const obj2 = getError(new HttpError());
+  expect(typeof obj2, "object");
+});
 test("Router method is function", () => {
   methods.forEach((method) => {
     // deno-lint-ignore no-explicit-any
@@ -48,7 +89,18 @@ router.c_routes.forEach(({ method, path }: TObject) => {
 });
 
 const app = new NHttp();
-app.use(router);
+app.use("/", router);
+app.use(fn);
+app.use("/assets", fn);
+
+test("Middleware length 1", () => {
+  expect(app.midds.length, 1);
+});
+
+test("Assets middleware length 2", () => {
+  const len = (app.pmidds as TObject)["/assets"].length;
+  expect(len, 2);
+});
 
 // convert fns [[[fn], fn], fn] to [fn, fn, fn] with recursive
 for (const route in app.route) {
@@ -116,4 +168,127 @@ METHOD_URL_MATCH.forEach(([method, url]) => {
       expect(obj.params.id, undefined);
     });
   }
+});
+
+test("query params", () => {
+  const obj = parseQuery("name=john&name=doe");
+  const obj2 = parseQuery("name[firstName]=john 1%%&name[lastName]=doe 2%%");
+  const obj3 = parseQuery("location[][lat]=123&location[][lat]=456");
+  expect(obj, { name: ["john", "doe"] });
+  expect(obj2, {
+    name: {
+      firstName: "john 1%%",
+      lastName: "doe 2%%",
+    },
+  });
+  expect(obj3, {
+    location: [
+      { lat: "123" },
+      { lat: "456" },
+    ],
+  });
+});
+
+test("cookie", () => {
+  const now = Date.now();
+  const date = new Date();
+  const cookie = serializeCookie("__Secure", "value", {
+    maxAge: now,
+    secure: true,
+    sameSite: "Lax",
+    domain: "xxx.com",
+    expires: date,
+    httpOnly: true,
+    path: "/",
+    other: ["other"],
+    encode: true,
+  });
+  serializeCookie("__Host", "value");
+  const obj = getReqCookies(
+    new Request("http://x.x/", {
+      headers: new Headers({ "Cookie": cookie }),
+    }),
+    true,
+  );
+  const obj2 = getReqCookies(
+    new Request("http://x.x", {
+      headers: new Headers({
+        "Cookie": serializeCookie("test", 'j:{ "name": "john" }', {
+          encode: true,
+        }),
+      }),
+    }),
+    true,
+  );
+  const obj3 = getReqCookies(
+    new Request("http://x.x", {
+      headers: new Headers({
+        "Cookie": serializeCookie("test", 'j:[{ "name": "john" }]', {
+          encode: true,
+        }),
+      }),
+    }),
+    true,
+  );
+  expect(obj, {
+    __Secure: "value",
+    Secure: "",
+    HttpOnly: "",
+    "Max-Age": now.toString(),
+    Domain: "xxx.com",
+    SameSite: "Lax",
+    Path: "/",
+    Expires: date.toUTCString(),
+    other: "",
+  });
+  expect(typeof obj2, "object");
+  expect(typeof obj3, "object");
+  expect(typeof cookie, "string");
+});
+
+const myApp = new NHttp();
+
+const sleep = (ms: number) => new Promise((ok) => setTimeout(ok, ms));
+
+myApp.get("/hello", () => "hello");
+// handle upload
+myApp.post(
+  "/upload",
+  multipart.upload({
+    name: "myfile",
+    required: true,
+    accept: "txt",
+    dest: "dummy/",
+    maxCount: 1,
+    maxSize: "2 mb",
+    callback: (file) => {
+      file.filename = "text.txt";
+    },
+  }),
+  ({ response }) => {
+    response.status(201);
+    return "success";
+  },
+);
+
+const info = await new Promise((ok) => {
+  myApp.listen({ port: 8080 }, (_, info) => {
+    ok(info);
+  });
+});
+await sleep(2000);
+const myRes = await fetch("http://localhost:8080/hello");
+
+const file = new File(["hello world"], "text.txt");
+const form = new FormData();
+form.append("myfile", file);
+form.append("text", "hello");
+const myUpload = await fetch("http://localhost:8080/upload", {
+  method: "POST",
+  body: form,
+});
+test("it should listen app", () => {
+  expect(myRes.status, 200);
+  expect(myUpload.status, 201);
+  expect((info as TObject).port, 8080);
 });
