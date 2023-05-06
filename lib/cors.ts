2ba057c4ff4c8554cfb6c1823d5b424fc0b5bdf4
herudi <herudi7@gmail.com>
2023-05-06 13:20:41 +0700

    add cors to lib

---
+++ lib/cors.ts
@@ -0,0 +1,44 @@
+import { Handler, TRet } from "./deps.ts";
+
+type tOptions = {
+  origin?: string | string[] | boolean;
+  credentials?: boolean;
+  allowHeaders?: string | string[];
+  allowMethods?: string | string[];
+  customHeaders?: Record<string, string>;
+  optionsStatus?: number;
+};
+
+export const cors = (opts: tOptions = {}): Handler => (rev, next) => {
+  if (opts.origin !== false) {
+    if (opts.origin === true) opts.origin = "*";
+    rev.response.setHeader(
+      "access-control-allow-origin",
+      opts.origin?.toString() ?? "*",
+    );
+  }
+  opts.optionsStatus ??= 204;
+  if (opts.credentials) {
+    rev.response.setHeader("access-control-allow-credentials", "true");
+  }
+  if (opts.allowHeaders) {
+    rev.response.setHeader(
+      "access-control-allow-headers",
+      opts.allowHeaders.toString(),
+    );
+  }
+  if (opts.allowMethods) {
+    rev.response.setHeader(
+      "access-control-allow-methods",
+      opts.allowMethods.toString(),
+    );
+  }
+  if (opts.customHeaders) rev.response.header(opts.customHeaders);
+  if (rev.request.method === "OPTIONS") {
+    rev.response.status(opts.optionsStatus);
+    return <TRet> null;
+  }
+  return next();
+};
+
+export default cors;
