defc1a3aa0737884864e6ebcd61b5d3e85d25bad
herudi <herudi7@gmail.com>
2023-05-07 15:44:07 +0700

    npm

--- npm/dist/esm/lib/cors.js
+++ npm/dist/esm/lib/cors.js
@@ -5,18 +5,24 @@ const cors = (opts = {}) => (rev, next) => {
     rev.response.setHeader("access-control-allow-origin", opts.origin?.toString() ?? "*");
   }
   opts.optionsStatus ??= 204;
+  opts.preflight ??= false;
   if (opts.credentials) {
     rev.response.setHeader("access-control-allow-credentials", "true");
   }
-  if (opts.allowHeaders) {
-    rev.response.setHeader("access-control-allow-headers", opts.allowHeaders.toString());
-  }
-  if (opts.allowMethods) {
-    rev.response.setHeader("access-control-allow-methods", opts.allowMethods.toString());
+  if (opts.exposeHeaders) {
+    rev.response.setHeader("access-control-expose-headers", opts.exposeHeaders.toString());
   }
   if (opts.customHeaders)
     rev.response.header(opts.customHeaders);
   if (rev.request.method === "OPTIONS") {
+    opts.allowMethods ??= "GET,HEAD,PUT,PATCH,POST,DELETE";
+    rev.response.setHeader("access-control-allow-headers", opts.allowHeaders?.toString() ?? rev.request.headers.get("access-control-request-headers") ?? "*");
+    rev.response.setHeader("access-control-allow-methods", opts.allowMethods.toString());
+    if (opts.maxAge) {
+      rev.response.setHeader("access-control-max-age", opts.maxAge.toString());
+    }
+    if (opts.preflight)
+      return next();
     rev.response.status(opts.optionsStatus);
     return null;
   }
--- npm/dist/types/lib/cors.d.ts
+++ npm/dist/types/lib/cors.d.ts
@@ -4,8 +4,11 @@ type TOptions = {
     credentials?: boolean;
     allowHeaders?: string | string[];
     allowMethods?: string | string[];
+    exposeHeaders?: string | string[];
     customHeaders?: Record<string, string>;
     optionsStatus?: number;
+    maxAge?: number;
+    preflight?: boolean;
 };
 export declare const cors: (opts?: TOptions) => Handler;
 export default cors;
