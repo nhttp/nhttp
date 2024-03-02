e558e646470ca990f243b5d3e319143b4298fbe5
Nassim Zen <hironichu@pm.me>
2024-03-02 01:26:36 +0100

    Removed ViewAsync decorator

--- lib/controller.ts
+++ lib/controller.ts
@@ -95,30 +95,17 @@ export function addMethod(method: string, path?: string | RegExp): TDecorator {
 export function View(name: string | TString): TDecorator {
   return (target: TObject, prop: string, des: PropertyDescriptor) => {
     const className = target.constructor.name;
-    const viewFn: Handler = (rev, next) => {
+    const viewFn: Handler = async (rev, next) => {
       const index = typeof name === "function" ? name(rev, next) : name;
       const fns = globalThis.NHttpMetadata[className]["route"][prop]["fns"];
       const body = fns[fns.length - 1](rev, next);
-      return rev.response.render(index, typeof body === "object" ? body : {});
+      return await rev.response.render(index, typeof body === "object" ? body : {});
     };
     joinHandlers(className, prop, [viewFn]);
     return des;
   };
 }
-export function ViewAsync(name: string | TString): TDecorator {
-  return (target: TObject, prop: string, des: PropertyDescriptor) => {
-      const className = target.constructor.name;
-      const viewFn: Handler = async (rev, next) => {
-      const index = typeof name === "function" ? name(rev, next) : name;
-      const fns = globalThis.NHttpMetadata[className]["route"][prop]["fns"];
-      const body = fns[fns.length - 1](rev, next);
-      const test = await rev.response.render(index, body instanceof Promise ? await body : typeof body === "object" ? body : {},);
-      return test;
-    };
-    joinHandlers(className, prop, [viewFn]);
-    return des;
-  };
-}
+
 export function Jsx(): TDecorator {
   return (target: TObject, prop: string, des: PropertyDescriptor) => {
     const className = target.constructor.name;
